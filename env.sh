88a0ebc34c4338e5ea0819182b8027c3d662b233
Nick Bargnesi <nick@den-4.com>
2014-09-11 00:08:08 -0400

    CUSTOM_ENV -> CUSTOM_ENV_SH

--- env.sh
+++ env.sh
@@ -6,7 +6,7 @@ source "$DIR/.gosh.sh" || exit 1
 
 ### GENERAL ENV VARS ###
 default DIR             "$DIR"
-default CUSTOM_ENV      "$DIR/env.sh.custom"
+default CUSTOM_ENV_SH   "$DIR/env.sh.custom"
 
 ### THE GO SHELL ###
 default GOSH_SCRIPTS    "$DIR"/scripts
