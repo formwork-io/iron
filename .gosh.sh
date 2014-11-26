5b2557125851d7a4417acc7bb793da70c7029b66
Nick Bargnesi <nick@den-4.com>
2014-11-26 16:54:01 -0500

    add assert_env function

--- .gosh.sh
+++ .gosh.sh
@@ -18,3 +18,20 @@ function default {
     fi
 }
 
+# Returns 1 if the environment variable $1 is not set, 0 otherwise.
+# E.g.,
+#    assert_env PATH
+function assert_env {
+    if [ $# -ne 1 ]; then
+        echo "usage: assert_env <variable>"
+        echo "(got: $@)"
+        exit 1
+    fi
+    eval __=\$$1
+    if [ -z "$__" ]; then
+        echo "$1 is not set" 1>&2
+        return 1
+    fi
+    return 0
+}
+
