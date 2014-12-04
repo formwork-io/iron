381626865a8b8f3f9c1be5905add129a0de12996
Nick Bargnesi <nbargnesi@selventa.com>
2014-12-03 22:48:46 -0500

    use assert_source when reading env.sh.custom

--- env.sh
+++ env.sh
@@ -2,7 +2,9 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Pull in standard functions, e.g., default.
-source "$DIR/.gosh.sh" || exit 1
+source "$DIR/.gosh.sh" || return 1
+default CUSTOM_ENV_SH "$DIR/env.sh.custom"
+assert_source "$CUSTOM_ENV_SH" || return 1
 
 ### GENERAL ENV VARS ###
 default DIR             "$DIR"
@@ -10,8 +12,4 @@ default CUSTOM_ENV_SH   "$DIR/env.sh.custom"
 
 ### THE GO SHELL ###
 default GOSH_SCRIPTS    "$DIR"/scripts
-
-if [ -r "$CUSTOM_ENV_SH" ]; then
-    source "$CUSTOM_ENV_SH" || exit 1
-fi
 
