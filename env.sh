a379d2299671c6e1589138ece7f3a9be4a89a590
Nick Bargnesi <nick@den-4.com>
2015-02-10 21:26:12 -0500

    fix _ usage in root env.sh

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Pull in standard functions, e.g., default.
 source "$DIR/.gosh.sh" || return 1
 default CUSTOM_ENV_SH "$DIR/env.sh.custom"
-assert_source "$CUSTOM_ENV_SH" || return 1
+assert-source "$CUSTOM_ENV_SH" || return 1
 
 ### GENERAL ENV VARS ###
 default DIR             "$DIR"
