581cfbc7824092042ed392c9383d9420ddc1a595
Nick Bargnesi <nick@den-4.com>
2017-03-19 01:32:28 -0400

    remove IRON_SCRIPTS from example export

--- try-examples.sh
+++ try-examples.sh
@@ -2,11 +2,11 @@
 #
 # Executes from the top-level dir
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export DIR
 cd "${DIR}" || exit 1
 source env.sh || exit 1
 
 export IRON_CONTRIB="$DIR"/examples/contrib
-export IRON_SCRIPTS="$DIR"/examples
 
 rlwrap -H .ironhst scripts/fe.sh "$@"
 
