7a18f4ba032a56acc9ea090c5cceab625f5fcb49
Nick Bargnesi <nick@den-4.com>
2017-04-15 13:11:38 -0400

    add test script

---
+++ scripts/go-tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$dir/../fe"
+export GOPATH="$(pwd)"
+
+ln -s vendor src
+src_ln="$(pwd)/src"
+function on_exit {
+    rm "$src_ln" 
+}
+trap on_exit EXIT
+
+#go get -u github.com/onsi/ginkgo/ginkgo
+#go get -u github.com/onsi/gomega
+PATH="$GOPATH/bin:$PATH"
+go build
+ginkgo
+
