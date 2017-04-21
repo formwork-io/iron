d89a37af26f2052d171816c38324e421502c9037
Nick Bargnesi <nick@den-4.com>
2017-04-20 22:39:35 -0400

    expand lib tests

--- fe/lib_test.go
+++ fe/lib_test.go
@@ -1,14 +1,20 @@
 package main_test
 
-import (
-	. "github.com/formwork-io/iron/fe"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-)
+import "os"
+import "path"
+import . "github.com/formwork-io/iron/fe"
+import . "github.com/onsi/ginkgo"
+import . "github.com/onsi/gomega"
 
 var _ = Describe("Internal", func() {
+	var examples string
 
 	BeforeEach(func() {
+		cwd, err := os.Getwd();
+		if err != nil {
+			panic(err)
+		}
+		examples = path.Join(cwd, "..", "examples")
 	})
 
 	Describe("Extended help", func() {
@@ -23,6 +29,37 @@ var _ = Describe("Internal", func() {
 		Context("With a single argument", func() {
 			It("should match", func() {
 				Expect(IsSubmenuArgcall("1,1")).To(BeTrue())
+			})
+		})
+	})
+
+	Describe("Items", func() {
+		Context("With a single digit", func() {
+			It("should match", func() {
+				Expect(IsItem("1")).To(BeTrue())
+			})
+		})
+		Context("With double digits", func() {
+			It("should match", func() {
+				Expect(IsItem("11")).To(BeTrue())
+			})
+		})
+	})
+
+	Describe("Get longest script name", func() {
+		Context("With a single script", func() {
+			It("finds the only script", func() {
+				stanza := ScriptStanza{}
+				stanza.Name = "foobar"
+				stanzas := []ScriptStanza{stanza}
+				Expect(GetMaxScriptName(stanzas)).To(Equal(6))
+			})
+		})
+	})
+
+	Describe("Reading scripts", func() {
+		Context("Using examples", func() {
+			It("parses example one", func() {
 			})
 		})
 	})
