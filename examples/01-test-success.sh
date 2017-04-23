18fc1088a3be544fee442418982e3aea24f8aa4d
Nick Bargnesi <nick@den-4.com>
2017-04-23 00:42:54 -0400

    rev example scripts, drop extended help

--- examples/01-test-success.sh
+++ examples/01-test-success.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 # The next three lines are for the fe shell.
-export SCRIPT_NAME="test-success"
-export SCRIPT_HELP="Mimic a successful script."
-[[ "$GOGO_IRON_SOURCE" -eq 1 ]] && return 0
+export IRON_SCRIPT_NAME="test-success"
+export IRON_SCRIPT_HELP="Mimic a successful script."
 
 # Normal script execution starts here.
 echo -n "Pretending to do something... "
